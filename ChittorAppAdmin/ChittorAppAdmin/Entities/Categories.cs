6457ec7802cafce77d0510d91ca6cee140154a10
ShailiDashora <dashorashaili17@hotmail.com>
2016-02-07 15:01:36 +0530

    RemainingItems

---
+++ ChittorAppAdmin/ChittorAppAdmin/Entities/Categories.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChittorAPPAdmin.Entities
+{
+    public class Categories
+    {   
+        [Key] 
+        public string ID { get; set; }
+        [Required(ErrorMessage="Please Enter Category Name")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "Please Enter Date")]
+        public DateTime AddedDate { get; set; }
+        [Required(ErrorMessage = "Please Enter Date")]
+        public DateTime ModifiedDate { get; set; }
+        [Required]
+        public String AddedBy { get; set; }
+        [Required]
+        public String ModifiedBy { get; set; }
+
+    }
+}
