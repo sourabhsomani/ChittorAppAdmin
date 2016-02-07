6734a4408127ccd40216e6978cb185fa5c6a7e83
ShailiDashora <dashorashaili17@hotmail.com>
2016-02-07 15:35:57 +0530

    RegistrationAction

---
+++ ChittorAppAdmin/ChittorAppAdmin/Controllers/CategoryController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChittorAPPAdmin.Entities;
+using ChittorAPPAdmin.Models;
+
+namespace ChittorAPPAdmin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Category/
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        // GET: /Category/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = db.Categories.Find(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // GET: /Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Category/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Category")] Categories categories)
+        {
+            categories.ID = Guid.NewGuid().ToString();
+            categories.AddedDate = DateTime.Now;
+            categories.ModifiedDate = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(categories);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(categories);
+        }
+
+        // GET: /Category/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = db.Categories.Find(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: /Category/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Category,AddedDate,ModifiedDate,AddedBy,ModifiedBy")] Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categories).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categories);
+        }
+
+        // GET: /Category/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = db.Categories.Find(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: /Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Categories categories = db.Categories.Find(id);
+            db.Categories.Remove(categories);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
