54e3cfcafa4333cafc490cc74472368f72545438
sourabh somani <sourabh_somani2010@hotmail.com>
2016-02-07 16:37:33 +0530

    AreaController

---
+++ ChittorAppAdmin/ChittorAppAdmin/Controllers/AreaController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using ChittorAPPAdmin.Entities;
+using ChittorAPPAdmin.Models;
+
+namespace ChittorAPPAdmin.Controllers
+{
+    [Authorize]
+    public class AreaController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Area/
+        public ActionResult Index()
+        {
+            var areas = db.Areas.Include(a => a.District);
+            return View(areas.ToList());
+        }
+
+        // GET: /Area/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Area area = db.Areas.Find(id);
+            if (area == null)
+            {
+                return HttpNotFound();
+            }
+            return View(area);
+        }
+
+        // GET: /Area/Create
+        public ActionResult Create()
+        {
+            ViewBag.DistrictID = new SelectList(db.Districts, "ID", "DistrictName");
+            return View();
+        }
+
+        // POST: /Area/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AreaName,DistrictID")] Area area)
+        {
+            area.ID = Guid.NewGuid().ToString();
+            area.AreaModifiedBy = User.Identity.GetUserId();
+            area.AreaAddedBy = User.Identity.GetUserId();
+            area.AreaModifiedDate = DateTime.Now;
+            area.AreaAddedDate=DateTime.Now;
+            db.Areas.Add(area);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Area/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Area area = db.Areas.Find(id);
+            if (area == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DistrictID = new SelectList(db.Districts, "ID", "DistrictName", area.DistrictID);
+            return View(area);
+        }
+
+        // POST: /Area/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,AreaName,AreaAddedDate,AreaModifiedDate,AreaAddedBy,AreaModifiedBy,DistrictID")] Area area)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(area).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DistrictID = new SelectList(db.Districts, "ID", "DistrictName", area.DistrictID);
+            return View(area);
+        }
+
+        // GET: /Area/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Area area = db.Areas.Find(id);
+            if (area == null)
+            {
+                return HttpNotFound();
+            }
+            return View(area);
+        }
+
+        // POST: /Area/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Area area = db.Areas.Find(id);
+            db.Areas.Remove(area);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
