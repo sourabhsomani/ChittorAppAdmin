6cb2e1a2f0b3e74cbded454a2992a7007061b907
ShailiDashora <dashorashaili17@hotmail.com>
2016-02-07 14:38:45 +0530

    Project Added

---
+++ ChittorAppAdmin/ChittorAppAdmin/Models/IdentityModels.cs
@@ -0,0 +1,20 @@
+﻿using ChittorAPPAdmin.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace ChittorAPPAdmin.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+        public virtual DbSet<Categories> Categories { get; set; }
+    }
+}
---
+++ ChittorAppAdmin/ChittorAppAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChittorAPPAdmin.Startup))]
+namespace ChittorAPPAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
